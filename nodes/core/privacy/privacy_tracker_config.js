8ada20dee2a9a632173f3118d7fd9a6e600e5b9f
Mohanna Shahrad <59365092+mohannashahrad@users.noreply.github.com>
2024-03-23 11:23:14 -0400

    updated the tracker-config node with both policy and labeller sections

--- nodes/core/privacy/privacy_tracker_config.js
+++ nodes/core/privacy/privacy_tracker_config.js
@@ -1,9 +1,40 @@
 module.exports = function(RED) {
-    function PrivacyTrackerConfigurationNode(config) {
+    function TrackerConfig(config) {
         RED.nodes.createNode(this, config);
-        this.policyfilename = config.policyfilename;
-        this.labellerfilename = config.labellerfilename;
+        this.fileNameType = config.fileNameType || 'passFileName';
+        this.policyFileName = config.policyFileName || '';
+        this.rules = config.rules || [{ source: '', sink: '', action: '' }];
+
+        this.labellerFileType = config.labellerFileType || 'passFileName';
+        this.labellerFileName = config.labellerFileName || '';
+        this.labellers = config.labellers || [{ label: '', func: ''}];
+
+        var node = this;
+
+        node.on('input', function(msg) {
+            // Access node properties
+            var fileNameType = node.fileNameType;
+            var policyFileName = node.policyFileName;
+            var rules = node.rules;
+
+            var labellerFileType = node.labellerFileType;
+            var labellerFileName = node.labellerFileName;
+            var labellers = node.labellers;
+
+            // Perform desired operations based on the configuration
+            // For example, you can log the configuration to the console
+            console.log('Policy File Name Type:', fileNameType);
+            console.log('Policy File Name:', policyFileName);
+            console.log('Rules:', rules);
+
+            console.log('Labeller File Name Type:', labellerFileType);
+            console.log('Labeller File Name:', labellerFileName);
+            console.log('Label Injectors:', labellers);
+
+            // Send the modified message along
+            node.send(msg);
+        });
     }
 
-    RED.nodes.registerType("tracker-config", PrivacyTrackerConfigurationNode);
-}
+    RED.nodes.registerType('tracker-config', TrackerConfig);
+};
